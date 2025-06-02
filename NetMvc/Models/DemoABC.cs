23cf94aa371dd01175341501c7656b712e245b3c
Duy-170103 <dn825514@gmail.com>
2025-06-03 00:30:24 +0700

    CreateProject

--- NetMvc/Models/DemoABC.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace NetMvc.Models;
-
-public class DemoABC
-{
-    public int PersonID { get; set; }
-    public string Fullname { get; set; }
-    public float age { get; set; }
-}
